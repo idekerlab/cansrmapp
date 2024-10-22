6bb752a68431809b3e2632b1b9f6e645fc134585
Chris Churas <churas.camera@gmail.com>
2024-10-22 11:35:58 -0700

    added requests

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['cellmaps_utils']
+requirements = ['requests',
+                'cellmaps_utils']
 
 setup_requirements = [ ]
 
